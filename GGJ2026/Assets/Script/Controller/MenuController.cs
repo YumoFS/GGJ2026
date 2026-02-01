acb114d58c96b31ff43f64d3c044bba383217aed
panda-piglet <18875812593@163.com>
2026-02-01 15:36:20 +0800

    add SettingsUI

--- GGJ2026/Assets/Script/Controller/MenuController.cs
+++ GGJ2026/Assets/Script/Controller/MenuController.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MenuController : MonoBehaviour
 {
+    // 在 Inspector 面板中把刚才创建的 CreditsPopup 拖进来
+    public GameObject creditsPopup;
     public void StartGame()
     {
-        // "GameScene" 是你游戏主地图场景的名字，请确保与场景文件名一致
+        // "map" 是你游戏主地图场景的名字，请确保与场景文件名一致
         SceneManager.LoadScene("map");
     }
     public void Quitgame()
     {
         Application.Quit();
+    }
+    public void ToSettings()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+    // 打开制作人员窗口
+    public void OpenCredits()
+    {
+        creditsPopup.SetActive(true);
+    }
+
+    // 关闭窗口
+    public void CloseCredits()
+    {
+        creditsPopup.SetActive(false);
     }
 }
